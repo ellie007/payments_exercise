889de7c3fd24154f0341ed62aa48c104e98e02d7
Eleanor Lee <ellie007@gmail.com>
2016-10-19 14:53:06 -0500

    add controller specs for payment create/post

--- spec/controllers/payments_controller_spec.rb
+++ spec/controllers/payments_controller_spec.rb
@@ -10,6 +10,29 @@ RSpec.describe PaymentsController, type: :controller do
     end
   end
 
+  describe '#create' do
+    let(:loan) { Loan.create!(funded_amount: 100.0) }
+
+    it 'responds with a 201' do
+      post :create, loan_id: loan.id, amount: 10.0
+      expect(response).to have_http_status(:created)
+    end
+
+    context 'if the loan is not found' do
+      it 'responds with a 422' do
+        post :create, loan_id: 10000, amount: 10.0
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+
+    context 'if the amount is not valid' do
+      it 'responds with a 422' do
+        post :create, loan_id: loan.id, amount: -10.0
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
   describe '#show' do
     let(:loan) { Loan.create!(funded_amount: 100.0) }
     let(:payment) { Payment.create(loan_id: loan.id, amount: 10.0 ) }
